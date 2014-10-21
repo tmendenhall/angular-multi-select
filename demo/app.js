6147067b89ef2ffaff0ad07dfafc047f61676b16
Tyler Mendenhall <tylermendenhall@gmail.com>
2014-10-21 13:00:49 -0400

    add unidirectional mx test.

--- demo/app.js
+++ demo/app.js
@@ -8,7 +8,9 @@ var myApp = angular.module('myApp',['multi-select'])
     {roleId: 2, roleName: "Super User", roleDescription: "Ultimate power!",mx:[]},
     {roleId:3, roleName:"Unlimited Cosmic Power", roleDescription:"Ultimate power",mx:[4,5]},
     {roleId:4, roleName:"Itty Bitty Living Space", roleDescription:"a tiny bottle",mx:[3,5]},
-    {roleId:5, roleName:"Normal User", roleDescription:"a normal user",mx:[3,4]}
+    {roleId:5, roleName:"Normal User", roleDescription:"a normal user",mx:[3,4]},
+    {roleId:6,roleName:"Exclusive",roleDescription:"mutually exclusive test",mx:[1,2]}
+
   ];
   
   $scope.user = {
